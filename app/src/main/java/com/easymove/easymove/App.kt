d6ab711134a8cbb11b10f2022084114295cfd708
Antoine <gm.anqo@gmail.com>
2020-06-24 22:18:28 +0200

    feat(history): detect close beacon

--- app/src/main/java/com/easymove/easymove/App.kt
+++ app/src/main/java/com/easymove/easymove/App.kt
@@ -1,18 +1,30 @@
 package com.easymove.easymove
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
 import com.easymove.easymove.auth.authModule
 import com.easymove.easymove.history.historyModule
 import com.easymove.easymove.shared.modules.network.ConnectivityObserver
 import com.easymove.easymove.shared.modules.network.networkModule
 import com.easymove.easymove.shared.utils.utilsModule
+import org.altbeacon.beacon.*
+import org.altbeacon.beacon.startup.BootstrapNotifier
+import org.altbeacon.beacon.startup.RegionBootstrap
 import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.fragment.koin.fragmentFactory
 import org.koin.core.context.startKoin
 
-class App : Application() {
 
+class App : Application(), BeaconConsumer, BootstrapNotifier, RangeNotifier {
+
+    private lateinit var beaconManager: BeaconManager
     private val connectivityObserver: ConnectivityObserver by inject()
 
     override fun onCreate() {
@@ -28,5 +40,73 @@ class App : Application() {
         }
 
         connectivityObserver.listenToConnectivityChange()
+
+        beaconManager = BeaconManager.getInstanceForApplication(this)
+
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0,
+            Intent(this, this::class.java), PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val notification = NotificationCompat.Builder(this, BuildConfig.APPLICATION_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Scanning for Beacons")
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                "My Notification Channel ID",
+                "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT
+            )
+            channel.description = "My Notification Channel Description"
+            val notificationManager = getSystemService(
+                Context.NOTIFICATION_SERVICE
+            ) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+            notification.setChannelId(channel.id)
+        }
+
+        beaconManager.enableForegroundServiceScanning(notification.build(), 121)
+        beaconManager.setEnableScheduledScanJobs(false)
+        beaconManager.backgroundBetweenScanPeriod = 0
+        beaconManager.backgroundScanPeriod = 1100
+
+        beaconManager.bind(this)
+
+        val region = Region(
+            "Gare Montparnasse",
+            null, null, null
+        )
+        RegionBootstrap(this, region)
+    }
+
+    override fun didDetermineStateForRegion(p0: Int, p1: Region?) {
+        println("§ didDetermineStateForRegion")
+    }
+
+    override fun didEnterRegion(p0: Region?) {
+        p0?.let {
+            println("§ Enter region ${it.uniqueId}")
+            beaconManager.startRangingBeaconsInRegion(it)
+            beaconManager.addRangeNotifier(this)
+        }
+    }
+
+    override fun didRangeBeaconsInRegion(beacons: MutableCollection<Beacon>?, region: Region?) {
+        beacons?.forEach { beacon ->
+            if (beacon.distance < 0.7) {
+                println("§ Beacon a moins de 70cm")
+            }
+        }
+    }
+
+    override fun didExitRegion(p0: Region?) {
+        p0?.let {
+            println("§ didExitRegion ${it.uniqueId}")
+        }
+    }
+
+    override fun onBeaconServiceConnect() {
+        println("§ onBeaconServiceConnect")
     }
 }
